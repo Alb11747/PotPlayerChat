8b9241002361f63d12b3740163413f81496c2a58
Alb11747 <albertli_042@hotmail.com>
2025-08-24 22:40:01 -0400

    fix: Remove special handing for tall Twitch emotes

--- src/renderer/src/components/Emote.svelte
+++ src/renderer/src/components/Emote.svelte
@@ -24,21 +24,12 @@
     enableEmotePreviews: boolean
   } = $props()
 
-  let emoteElement: HTMLImageElement | null = $state(null)
-
   const emoteSizeMap: Record<string, string> = {
     '1': '0.5x',
     '2': '1x',
     '3': '1.5x',
     '4': '2x'
   }
-
-  const isTallTwitchEmote = $derived.by(() => {
-    if (!emoteElement) return false
-    if (segment.type !== 'emote') return false
-    if (segment.emote.type !== 'twitch') return false
-    return emoteElement.naturalHeight > emoteElement.naturalWidth
-  })
 
   function mouseUpdateEmote(segment: Segment & { type: 'emote' | 'cheer' }): void {
     if (enableEmotePreviews && !currentPreviewType()) {
@@ -66,9 +57,7 @@
   }}
 >
   <img
-    bind:this={emoteElement}
     class="chat-emote"
-    class:tall-twitch-emote={isTallTwitchEmote}
     srcset={urlsToSrcset(segment.urls, emoteSizeMap)}
     alt={segment.name}
     loading="lazy"
@@ -121,13 +110,8 @@
     margin-right: 0.25rem;
     font-weight: 900;
   }
-
   .zero-width-emote {
     z-index: 1;
-  }
-
-  .tall-twitch-emote {
-    max-height: 1.6rem;
   }
 
   .cheer-emote-container {
